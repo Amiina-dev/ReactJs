81eae3fd4f8d89e54956a32e53380a2c6a90baa1
Amab <miynaoje@outlook.com>
2024-05-28 13:01:28 +0100

    Changes

---
+++ reactproject/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import Accordion from "./components/accordion";
+import RandomColor from "./components/randomcolour";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordion component */}
+      <Accordion />
+
+      {/* Random Color Component*/}
+      {/* <RandomColor /> */}
+
+      {/* Star Rating Component */}
+      {/* <StarRating noOfStars={6} /> */}
+
+      {/* Image Slider */}
+      {/* <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      /> */}
+    </div>
+  );
+}
+
+export default App;
